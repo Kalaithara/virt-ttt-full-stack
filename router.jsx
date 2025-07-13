1c0ec7e63026be261244d73be3b9a0e0cf204dca
Kalaithara <kalaivani.r@skct.edu.in>
2025-07-14 00:58:49 +0530

    Update router.jsx

--- router.jsx
+++ router.jsx
@@ -1,10 +1,31 @@
 import { useRoutes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Addition } from "./Addition";
+import Classcomponent from "./Classcomponent";
+import Counter from "./Counterextension";
+import UserCard from "./Usercard";
+import InputLogger from "./Inputfield";
+import LiveInputDisplay from "./Textinput";
+import FruitList from "./Listoffruits";
+import LoggerComponent from "./Logmsg";
+import Button from "./Buttons";
+import HoverButton from "./Buttons";
 
 export default function AppRouter() {
   return useRoutes([
     { path: "/", element: <Welcome name={"Kavi"} /> },
     { path: "/add", element: <Addition /> },
+    { path: "/class", element: <Classcomponent /> },
+    { path: "/counter", element: <Counter /> },
+    { path: "/usercard", element: <UserCard name="Kalai" age={30} /> },
+    { path: "/inputlogger", element: <InputLogger /> },
+    { path: "/liveinput", element: <LiveInputDisplay /> },
+    {
+      path: "/fruits",
+      element: <FruitList fruits={["Apple", "Orange", "Cherry"]} />,
+    },
+    { path: "/logmsg", element: <LoggerComponent /> },
+    { path: "/button", element: <Button /> },
+    { path: "/hoverbutton", element: <HoverButton /> },
   ]);
 }
